a92932d9d996fa1a91a0f5e041c14bed9c16acb4
Leonard de Ruijter <alderuijter@gmail.com>
2022-03-19 19:45:10 +0100

    Revert install winget update

--- Install-Winget.ps1
+++ Install-Winget.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 
 Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibs.appx
 Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00.UWPDesktop_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibsuwp.appx
-Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile $env:TEMP\winget.msixbundle
-Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/b0a0692da1034339b76dce1c298a1e42_License1.xml' -OutFile $env:TEMP\winget.license
+Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile $env:TEMP\winget.msixbundle
+Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/9c0fe2ce7f8e410eb4a8f417de74517e_License1.xml' -OutFile $env:TEMP\winget.license
 
 Add-AppxProvisionedPackage -Online -PackagePath $env:TEMP\winget.msixbundle -LicensePath $env:TEMP\winget.license -DependencyPackagePath @("$env:TEMP\vclibs.appx", "$env:TEMP\vclibsuwp.appx")
