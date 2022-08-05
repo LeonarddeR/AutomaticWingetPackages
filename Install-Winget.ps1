0738d65af80578e761777c0abb644f6330aeb571
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2022-08-05 15:26:54 +0200

    Verbose winget install

--- Install-Winget.ps1
+++ Install-Winget.ps1
@@ -1,5 +1,7 @@
 # From https://github.com/actions/virtual-environments/issues/910#issuecomment-1009640391
 $ErrorActionPreference = 'Stop'
+$VerbosePreference = "Continue"
+$DebugPreference = "Continue"
 
 Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibs.appx
 Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00.UWPDesktop_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibsuwp.appx
