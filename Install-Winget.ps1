73866dc926b53a009f97646e925ba0014a691a98
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2022-09-19 10:44:09 +0200

    Improve install winget script

--- Install-Winget.ps1
+++ Install-Winget.ps1
@@ -1,11 +1,19 @@
 # From https://github.com/actions/virtual-environments/issues/910#issuecomment-1009640391
 $ErrorActionPreference = 'Stop'
-$VerbosePreference = "Continue"
-$DebugPreference = "Continue"
+$VerbosePreference = 'Continue'
+$DebugPreference = 'Continue'
+Set-StrictMode -Version latest
 
-Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibs.appx
-Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.VCLibs.140.00.UWPDesktop_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\vclibsuwp.appx
-Invoke-WebRequest $(Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body 'type=PackageFamilyName&url=Microsoft.UI.Xaml.2.7_8wekyb3d8bbwe&ring=RP&lang=en-US' -UseBasicParsing | ForEach-Object Links | Where-Object outerHTML -Match 'Microsoft.UI.Xaml.2.7_.+_x64__8wekyb3d8bbwe.appx' | ForEach-Object href) -OutFile $env:TEMP\xaml.appx
+function Get-PackageUriFromAdguard([String]$package, $architecture = 'x64') {
+	$packageSplit = $package.Split('_')
+	$packageMatch = "$($packageSplit[0])_.+_$($architecture)__$($packageSplit[1]).appx"
+	$res = Invoke-WebRequest 'https://store.rg-adguard.net/api/GetFiles' -Method 'POST' -ContentType 'application/x-www-form-urlencoded' -Body "type=PackageFamilyName&url=$package&ring=RP&lang=en-US" -UseBasicParsing
+	return $res | ForEach-Object Links | Where-Object outerHTML -Match $packageMatch | ForEach-Object href
+}
+
+Invoke-WebRequest -Uri (Get-PackageUriFromAdguard('Microsoft.VCLibs.140.00_8wekyb3d8bbwe')) -OutFile $env:TEMP\vclibs.appx
+Invoke-WebRequest -Uri (Get-PackageUriFromAdguard('Microsoft.VCLibs.140.00.UWPDesktop_8wekyb3d8bbwe')) -OutFile $env:TEMP\vclibsuwp.appx
+Invoke-WebRequest -Uri (Get-PackageUriFromAdguard('Microsoft.UI.Xaml.2.7_8wekyb3d8bbwe')) -OutFile $env:TEMP\xaml.appx
 Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.3.2091/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle' -OutFile $env:TEMP\winget.msixbundle
 Invoke-WebRequest 'https://github.com/microsoft/winget-cli/releases/download/v1.3.2091/7b91bd4a0be242d6aa8e8da282b26297_License1.xml' -OutFile $env:TEMP\winget.license
 
