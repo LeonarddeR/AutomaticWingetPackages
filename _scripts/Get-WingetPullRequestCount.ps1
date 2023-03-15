f89b0333bc00224b5870769b4a1071ede2816951
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2023-03-15 14:32:23 +0100

    Fix 1 password beta

--- _scripts/Get-WingetPullRequestCount.ps1
+++ _scripts/Get-WingetPullRequestCount.ps1
@@ -6,10 +6,16 @@ function Get-WingetPullRequestCount {
 		$PackageName,
 		[Parameter(Mandatory)]
 		[string]
-		$Version
+		$Version,
+		[string]
+		$AdditionalCriteria
 	)
 	Write-Information "Getting pull request for package $($PackageName) version $($Version)"
-	$result = Invoke-RestMethod -Uri "https://api.github.com/search/issues?q=repo%3Amicrosoft%2Fwinget-pkgs+is%3Apr+`"$PackageName+$Version`"+in%3Atitle" -Headers @{Accept = 'application/vnd.github.v3.text-match+json' }
+	$uri = "https://api.github.com/search/issues?q=repo%3Amicrosoft%2Fwinget-pkgs+is%3Apr+`"$PackageName+$Version`"+in%3Atitle"
+	if (-not [String]::IsNullOrWhiteSpace($AdditionalCriteria)) {
+		$uri += "+$([System.Web.HttpUtility]::UrlEncode($AdditionalCriteria))"
+	}
+	$result = Invoke-RestMethod -Uri $uri -Headers @{Accept = 'application/vnd.github.v3.text-match+json' }
 	$count = $result.total_count
 	Write-Information "Found $($count) pull requests for $($PackageName) $($Version)"
 	return $count
