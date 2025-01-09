f2d6b7db682430fb94960c927437af52f360fab7
Leonard de Ruijter <l.de.ruijter@sevenp.nl>
2025-01-09 11:50:14 +0100

    Fixing

--- NVAccess.NVDA/update.ps1
+++ NVAccess.NVDA/update.ps1
@@ -1,7 +1,8 @@
 . $PSScriptRoot\..\_scripts\all.ps1
 
 $packageName = 'NVAccess.NVDA'
-$wingetVersion = (Get-WingetLastPackageVersion $packageName)
+$wingetPackage = (Get-WinGetPackage -Id $packageName)
+$wingetVersion = $wingetPackage.AvailableVersions[0]
 $url = "https://www.nvaccess.org/nvdaUpdateCheck?autoCheck=False&allowUsageStats=False&version=$wingetVersion&versionType=stable&osVersion=10.0.17763&x64=True"
 Write-Information "Getting most recent version from NVAccess using $($wingetVersion) as reference version"
 $result = Invoke-RestMethod -Uri $url
