ede449a86eece54ed6e78966dc9d8c18663e5f1a
Leonard de Ruijter <alderuijter@gmail.com>
2025-02-06 10:21:01 +0100

    FIxup 1password

--- AgileBits.1Password/update.ps1
+++ AgileBits.1Password/update.ps1
@@ -1,13 +1,23 @@
+[CmdletBinding()]
+param(
+    [String]$ForceVersion = $null
+)
 . $PSScriptRoot\..\_scripts\all.ps1
 
 $packageName = 'AgileBits.1Password'
-$wingetVersion = (Get-WingetLastPackageVersion $packageName)
-$latestExeUrl = 'https://downloads.1password.com/win/1PasswordSetup-latest.exe'
-$localExePath = "$($env:TEMP)\1PasswordSetup-latest.exe"
-Write-Information "Downloading most recent executable for $($PackageName)"
-Invoke-RestMethod -Uri $latestExeUrl -OutFile $localExePath
-$mostRecentVersion = (Get-ItemProperty $localExePath).VersionInfo.ProductVersion
+$wingetPackage = (Find-WinGetPackage -Id $packageName -Count 1)
+$wingetVersion = $wingetPackage.Version
+if ($Null -Eq $ForceVersion) {
+    $latestExeUrl = 'https://downloads.1password.com/win/1PasswordSetup-latest.exe'
+    $localExePath = "$($env:TEMP)\1PasswordSetup-latest.exe"
+    Write-Information "Downloading most recent executable for $($PackageName)"
+    Invoke-RestMethod -Uri $latestExeUrl -OutFile $localExePath
+    $mostRecentVersion = ((Get-ItemProperty $localExePath).VersionInfo.ProductVersion -split '\.')[0..2] -join '.'
+}
+else {
+    $mostRecentVersion = $ForceVersion
+}
 Write-Information "Most recent version downloaded is version $($mostRecentVersion ). Comparing against version $($wingetVersion) in WinGet repository"
-if ([Version]$mostRecentVersion -Gt [Version]$wingetVersion -and (Get-WingetPullRequestCount $packageName $mostRecentVersion -AdditionalCriteria "NOT beta in:title") -Eq 0) {
+if ([Version]$mostRecentVersion -Gt [Version]$wingetVersion -and (Get-WingetPullRequestCount $packageName $mostRecentVersion -AdditionalCriteria 'NOT beta in:title') -Eq 0) {
     Publish-WingetPackagePullRequest -PackageName $packageName -Version $mostRecentVersion -urls https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).exe, https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).msi
 }
