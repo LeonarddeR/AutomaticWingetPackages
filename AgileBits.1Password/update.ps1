ec7f380885cc3595c1b0eff34a87c0f6206089ff
Leonard de Ruijter <alderuijter@gmail.com>
2025-02-19 19:16:48 +0100

    Extra check

--- AgileBits.1Password/update.ps1
+++ AgileBits.1Password/update.ps1
@@ -19,7 +19,11 @@ else {
 }
 Write-Information "Most recent version downloaded is version $($mostRecentVersion ). Comparing against version $($wingetVersion) in WinGet repository"
 if ([Version]$mostRecentVersion -Gt [Version]$wingetVersion -and (Get-WingetPullRequestCount $packageName $mostRecentVersion -AdditionalCriteria 'NOT beta in:title') -Eq 0) {
-    $urls = @("https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).exe", "https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).msi")
+    $urls = @(
+        "https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).exe",
+        "https://downloads.1password.com/win/1PasswordSetup-$($mostRecentVersion).msi",
+        "https://downloads.1password.com/win/arm64/1PasswordSetup-$($mostRecentVersion)-arm64.exe"
+    )
     foreach ($url in $urls) {
         try {
             Invoke-WebRequest -Uri $url -Method Head -ErrorAction Stop | Out-Null
