01c4e090a8d43a05632e2182c9efc3c6c6dc75ea
Leonard de Ruijter <alderuijter@gmail.com>
2025-05-24 09:13:37 +0200

    NVDA Beta

--- NVAccess.NVDA/update.ps1
+++ NVAccess.NVDA/update.ps1
@@ -7,10 +7,10 @@ $url = "https://api.nvaccess.org/nvdaUpdateCheck?autoCheck=False&allowUsageStats
 Write-Information "Getting most recent version from NVAccess using $($wingetVersion) as reference version"
 $result = Invoke-RestMethod -Uri $url
 if ($result -Ne [String]::Empty) {
-    $mostRecentVersion = ($result | Select-String 'version: (.+)').Matches.Groups[1].Value
+    $mostRecentVersion = ($result | Select-String '\sversion: (.+)').Matches.Groups[1].Value
     Write-Information "Most recent version available is version $($mostRecentVersion)"
     if ((Get-WingetPullRequestCount $packageName $mostRecentVersion) -Eq 0) {
-        $launcherUrl = ($result | Select-String 'launcherUrl: (.+)\?update=1').Matches.Groups[1].Value
+        $launcherUrl = ($result | Select-String 'launcherUrl: (.+)').Matches.Groups[1].Value
         Publish-WingetPackagePullRequest -PackageName $packageName -Version $mostRecentVersion -urls $launcherUrl -Submit
     }
 }
